fcff33c4d9f62841de4e985a9c8e05e010af2127
colonP <51826774+colonP@users.noreply.github.com>
2022-04-21 21:22:09 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 (async() => {
-  await import('./server.mjs').then(e=>e.default({
-    cookie: false, // Cookie Authentication
-    primaryProxy: 'ultraviolet', // Main Proxy on Enter
-    googleBlock: true, // Block Google's Bots
-    cors: true, // Allow Cross-Origin Requests
-    config: {
-      port: 8080, // Port to host site on (Default: 8080)
-      gamePort: 3000, // Port for games (Default: 3000) (NOT IMPLEMENTED)
-    }
-  }));
+  await import('./index.mjs');
 })();
